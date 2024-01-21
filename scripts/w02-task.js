de088d781c29a7e0029bc5671095a95bbaa2d6d2
PalmerGiblette <palmerdavidg@yahoo.com>
2024-01-20 20:55:14 -0700

    first commit

---
+++ scripts/w02-task.js
@@ -0,0 +1,34 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName = 'Palmer Giblette';
+
+const y = new Date();
+let currentYear = y.getFullYear();
+
+
+let picture = 'images/pic.png'
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
